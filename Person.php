4eb9dd42aada10139e913c853f1ef55ce0d5c351
CelLine00 <153966349+CelLine00@users.noreply.github.com>
2024-11-08 01:23:03 +0800

    Update Person.php

--- Person.php
+++ Person.php
@@ -1,10 +1,10 @@
 <?php
 
 class Person {
-    private $name;
-    private $address;
-    private $age;
-    private $companyName;
+    protected $name;
+    protected $address;
+    protected $age;
+    protected $companyName;
 
     public function __construct($name, $address, $age, $companyName) {
         $this->name = $name;
@@ -29,26 +29,11 @@ class Person {
         return $this->companyName;
     }
 
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function setAddress($address) {
-        $this->address = $address;
-    }
-
-    public function setAge($age) {
-        $this->age = $age;
-    }
-
-    public function setCompanyName($companyName) {
-        $this->companyName = $companyName;
-    }
-
     public function __toString() {
-        return "Name: $this->name, Address: $this->address, Age: $this->age, Company Name: $this->companyName";
+        return $this->name . " | " . $this->address . " | " . $this->age . " | " . $this->companyName;
     }
 }
+?>
         
 
     
